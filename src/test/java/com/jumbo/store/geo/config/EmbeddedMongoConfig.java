503e9a0e72323891e98c0764eb311f5854d79e09
Mohsen <mohsen.zadmehr@gmail.com>
2024-12-09 00:00:30 +0330

    test

--- src/test/java/com/jumbo/store/geo/config/EmbeddedMongoConfig.java
+++ src/test/java/com/jumbo/store/geo/config/EmbeddedMongoConfig.java
@@ -5,11 +5,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import com.mongodb.client.MongoClients;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.MongodConfig;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
 
 @TestConfiguration
 public class EmbeddedMongoConfig {
+
     @Bean
-    public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(MongoClients.create("mongodb://localhost:27017"), "testdb");
+    public MongodExecutable embeddedMongoServer() throws Exception {
+        MongodStarter starter = MongodStarter.getDefaultInstance();
+        int port = 27017;
+        MongodConfig mongodConfig = MongodConfig.builder()
+                .version(Version.Main.PRODUCTION)
+                .net(new Net(port, Network.localhostIsIPv6()))
+                .build();
+        return starter.prepare(mongodConfig);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongodExecutable mongodExecutable) throws Exception {
+        mongodExecutable.start();
+        return new MongoTemplate(MongoClients.create("mongodb://localhost:27019"), "testdb");
     }
 }
